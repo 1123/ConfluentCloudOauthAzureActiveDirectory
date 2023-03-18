8f101dac37f054174ddd5b22b90adfccc0508995
Benedikt Linse <benedikt@confluent.io>
2023-03-18 11:39:22 +0100

    simplification

--- run-producer.sh
+++ run-producer.sh
@@ -1,9 +1,5 @@
 kafka-console-producer \
-  --producer-property 'security.protocol=SASL_SSL' \
-  --producer-property sasl.oauthbearer.token.endpoint.url=https://login.microsoftonline.com/$AZURE_TENANT/oauth2/v2.0/token \
-  --producer-property sasl.login.callback.handler.class=org.apache.kafka.common.security.oauthbearer.secured.OAuthBearerLoginCallbackHandler \
-  --producer-property sasl.mechanism=OAUTHBEARER \
-  --producer-property "sasl.jaas.config=org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required clientId='$CLIENT_ID' scope='$CLIENT_ID/.default' clientSecret='$CLIENT_SECRET' extension_logicalCluster='$LOGICAL_CLUSTER_ID' extension_identityPoolId='$IDENTITY_POOL_ID';" \
+  --producer.config azure-ad-client.properties \
   --bootstrap-server $BOOTSTRAP_SERVERS \
   --topic t1
 
